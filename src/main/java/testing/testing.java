606ea33468de7ca6403ffe102bcc03ada9cd5177
colleenl <[lynch.colleen@gmail.com]>
2022-02-10 13:55:57 -0500

    stuff

---
+++ src/main/java/testing/testing.java
@@ -0,0 +1,19 @@
+package testing;
+
+import com.revature.models.Role;
+import com.revature.services.AuthService;
+
+public
+class testing {
+
+
+    public static
+    void main(String[] args) {
+        AuthService authService = new AuthService();
+
+//        authService.register(8, "user", "user", "user@user.com", 11,
+//                "1 user rd", "user", "user", Role.valueOf("EMPLOYEE"));
+//
+//    }
+    }
+}
